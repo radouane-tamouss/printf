6459075aae1746558fa1266ed70e76489aa360eb
mehdi <mehdilahrach89@gmail.com>
2023-07-24 05:11:22 -0700

    edit

--- 0-main.h
+++ 0-main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #define NULL ((void *)0)
 
+
 int _putchar(char c);
 int _print_str(char *string);
 int _print_int(long int var);
@@ -10,5 +11,6 @@ int to_Binary(unsigned int n);
 int to_Octal(unsigned int n);
 int _power_recursion(int x, int y);
 int to_Hexa(unsigned int num);
+
 
 #endif
